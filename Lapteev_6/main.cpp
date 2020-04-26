ec08768d94a3c5d80b4f4cc6b4cb2f0201246587
sampletext32 <sampletext32@bk.ru>
2020-04-26 17:07:44 +0300

    Add project

---
+++ Lapteev_6/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double min(double k, double m)
+{
+	double res;
+	if (k <= m)
+	{
+		res = k;
+	}
+	else
+	{
+		res = m;
+	}
+	return res;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	double x, y;
+	
+	cout << "Введите x: ";
+	cin >> x;
+
+	cout << "Введите y: ";
+	cin >> y;
+
+	double t = (min(x, y) - min(x * x, x - y)) / (1.2 + min(x * x, y * y));
+
+	cout << "x: " << x << ", y: " << y << ", t: " << t << endl;
+
+	system("pause");
+
+	return 0;
+}
